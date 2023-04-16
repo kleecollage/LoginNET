e61a794a7265f2f29d5a8e7d506b9f43defc8275
kleecollage <56179611+kleecollage@users.noreply.github.com>
2023-04-16 14:48:10 -0500

    Initial commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginNET.Models
+{
+    public class Usuario
+    {
+        public int Id_Usuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+
+        public string ConfirmarClave { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LoginNET
+ Login Creado en Visual Studio con ASP.NET MVC5 y con conección a una BD de Microsoft SQL Server local que incluye procedimientos almacenados
